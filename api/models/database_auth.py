33e48d1aa75bb3c9a302e8f9f5016ead1c2fff2c
Amir Mashayekhi <ap4games2017@gmail.com>
2025-03-21 03:43:07 +0330

    update database auth model and add database attr

--- api/models/database_auth.py
+++ api/models/database_auth.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class ConnectionParameter(models.Model):
+    database = models.CharField(max_length=100, blank=True, null=True)
     host = models.CharField(max_length=100, blank=True, null=True)
     port = models.CharField(max_length=100, blank=True, null=True)
     username = models.CharField(max_length=100, blank=True, null=True)
