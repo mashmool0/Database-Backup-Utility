a5d61dccf76745fc1c2b85fb6b39da0dfec5164b
Amir Mashayekhi <ap4games2017@gmail.com>
2025-04-12 21:19:09 +0330

    create a command for connect to api of connect databae

--- cli_tool/main.py
+++ cli_tool/main.py
@@ -1,23 +1,53 @@
 import click
+import requests
+import json
 
 
 @click.group()
 def cli():
+    """این برنامه برای مدیریت دیتابیس طراحی شده است"""
     pass
 
 
 @click.command()
-def hello():
-    click.echo("hi welcome to this tutorial!")
+@click.argument("database")
+@click.argument("host")
+@click.argument("password")
+@click.argument("port")
+@click.argument("username")
+def connectdb(username, host, password, port, database):
+    click.echo(f"Database: {database}")
+    click.echo(f"Host: {host}")
+    click.echo(f"Username: {username}")
+    click.echo(f"Password: {password}")
+    click.echo(f"Port: {port}")
+    url = 'http://localhost:8000/api/'
+    headers = {'Content-Type': 'application/json'}
+    data = {
+        'username': username,
+        'host': host,
+        'password': password,
+        'port': str(port),
+        'database': database
+    }
+    try:
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+        response.raise_for_status()
+
+        if response.status_code == 200:
+            print("API Call Was Successfully")
+            print("response : ", response.text)
+            click.echo("Database connected")
+        else:
+            click.echo(f"Connection Failed \nstatus code :{str(response.status_code)}\nmessage:{response.text}", )
 
 
-@click.command()
-def by():
-    click.echo("khodah negah daretooon farda miam pishetoon !")
+    except requests.exceptions.RequestException as e:
+        print(e)
+        raise None
 
 
-cli.add_command(hello)
-cli.add_command(by)
+cli.add_command(connectdb)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
